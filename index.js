f8eed4ced87f000216d3c2e16d1ca9a9515884ae
eminaydin <eminaydin93@yahoo.com>
2020-05-19 14:48:15 +0200

    Create new schema and delete one

--- index.js
+++ index.js
@@ -17,9 +17,25 @@ const taskSchema = new Schema({
     default: Date.now,
   },
 });
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "Tell me something about yourself"
+  }
+})
 
 // Models
 const Task = mongoose.model("Task", taskSchema);
+const User = mongoose.model("User", userSchema);
+
 
 const main = async () => {
   // Create new task and save it
@@ -30,21 +46,37 @@ const main = async () => {
   const sleeping = new Task({ name: "Sleeping", message: "Humans needs sleep too" })
   const eating = new Task({ name: "Eating", message: "You may die without food" })
 
-  chores, dishes, reading, sleeping, eating, newTask.save();
+  sleeping.save();
+  reading.save();
+  dishes.save();
+  chores.save();
+  eating.save();
+  newTask.save();
 
   // Find one Task
-  const goFind = await Task.findOne({ message: "Prio 1" });
-  console.log(goFind);
+  const goFind = await Task.findOne({ message: "You may die without food" });
+
 
   // Delete one task
+  Task.Task.deleteOne({ name: "Eating" })
+
 };
+const person = async () => {
+
+  const newUser = new User({ name: "Emin", age: 27, description: "Hello I'am Emin" })
+  newUser.save();
+
+  const findMe = await User.findOne({ name: "Emin" })
+  console.log(findMe);
+
+}
 
 mongoose
   .connect("mongodb://localhost:27017/fbw21", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(main)
+  .then(person, main)
   .finally(() => {
     //res.send()
     mongoose.connection.close();
