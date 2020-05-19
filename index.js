bcc7bc3ed9cd2d1b5e3bde48c9c8916c11ef2baf
eminaydin <eminaydin93@yahoo.com>
2020-05-19 14:03:15 +0200

    Create new tasks

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ const Task = mongoose.model("Task", taskSchema);
 const main = async () => {
   // Create new task and save it
   const newTask = new Task({ name: "New Task", message: "🎉" });
-  newTask.save();
+  const chores = new Task({ name: "Chores", message: "Clean the garden" })
+  const dishes = new Task({ name: "Dishes", message: "You should clean up the dishes" })
+  const reading = new Task({ name: "Reading", message: "You should be reading to stay educated" })
+  const sleeping = new Task({ name: "Sleeping", message: "Humans needs sleep too" })
+  const eating = new Task({ name: "Eating", message: "You may die without food" })
+
+  chores, dishes, reading, sleeping, eating, newTask.save();
 
   // Find one Task
   const goFind = await Task.findOne({ message: "Prio 1" });
